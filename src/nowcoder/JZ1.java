2dc02680c011cba64aa7f635bedd3d9417b62055
holdice <hemu0710@qq.com>
2021-01-05 13:24:37 +0800

    2020/1/5 JZ1 fix

--- src/nowcoder/JZ1.java
+++ src/nowcoder/JZ1.java
@@ -3,19 +3,34 @@ package nowcoder;
 public class JZ1 {
     // https://www.nowcoder.com/practice/abc3fe2ce8e146608e868a70efebf62e?tpId=13&&tqId=11154&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
     // 二维数组中的查找
+    /**
+     * 暴力查找，竟然也能过？？？
     public boolean Find(int target, int [][] array) {
-        int i = array.length - 1;
-        int j = array[0].length - 1;
-        do {
-            i --;
-            j --;
-        } while (target < array[i][j]);
-        for (int k = 0; k < i + 1; k++) {
-            if (array[k][j + 1] == target) return true;
-        }
-        for (int k = 0; k < j + 1; k++) {
-            if (array[i + 1][k] == target) return true;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (target == array[i][j]) return true;
+            }
         }
         return false;
     }
+    */
+
+    public boolean Find(int target, int [][] array) {
+        if (array.length == 0) return false;
+        if (array[0].length == 0) return false;
+        int jSize = array[0].length - 1;
+        int iSize = array.length - 1;
+        if (target < array[0][0] || target > array[iSize][jSize]) return false;
+        for (int i = 0; i <= iSize; i++) {
+            if (target == array[i][0] || target == array[i][jSize]) return true;
+            if (target > array[i][jSize]) continue;
+            for (int j = 0; j < jSize; j++) {
+                if (target == array[0][j] || target == array[iSize][j]) return true;
+                if (target > array[iSize][j]) continue;
+                if (target == array[i][j]) return true;
+            }
+        }
+        return false;
+    }
+
 }
