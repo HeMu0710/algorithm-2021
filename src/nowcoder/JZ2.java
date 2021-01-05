d40008696e9a656a5ec4586b951b9d6e936c6d23
holdice <hemu0710@qq.com>
2021-01-05 13:29:03 +0800

    2020/1/5 JZ2

---
+++ src/nowcoder/JZ2.java
@@ -0,0 +1,14 @@
+package nowcoder;
+
+public class JZ2 {
+    // https://www.nowcoder.com/practice/4060ac7e3e404ad1a894ef3e17650423?tpId=13&&tqId=11155&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+    // 替换空格
+    public String replaceSpace(StringBuffer str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                str.replace(i, i + 1, "%20");
+            }
+        }
+        return str.toString();
+    }
+}
