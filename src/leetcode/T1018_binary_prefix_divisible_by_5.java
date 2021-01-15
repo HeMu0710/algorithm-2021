8d56a7de3559e7adfd641df62397a582de48fae5
LAPTOP-HM\holdice <hemu0710@qq.com>
2021-01-15 20:54:03 +0800

    2020/1/15 fix 1/14

---
+++ src/leetcode/T1018_binary_prefix_divisible_by_5.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class T1018_binary_prefix_divisible_by_5 {
+    public List<Boolean> prefixesDivBy5(int[] A) {
+        List<Boolean> ans = new ArrayList<>();
+
+        int num = 0;
+        for (int i = 0;i < A.length;i++) {
+            num <<= 1;
+            num += A[i];
+            num %= 10;
+            ans.add(num % 5 == 0);
+        }
+        return ans;
+    }
+}
