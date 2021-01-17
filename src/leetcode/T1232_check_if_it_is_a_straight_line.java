495e78223b1ebd1a54f9942ad2fffcc57c7f84e8
LAPTOP-HM\holdice <hemu0710@qq.com>
2021-01-17 15:11:55 +0800

    2020/1/17 too easy

---
+++ src/leetcode/T1232_check_if_it_is_a_straight_line.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class T1232_check_if_it_is_a_straight_line {
+    public boolean checkStraightLine(int[][] coordinates) {
+        if (coordinates.length == 2) {
+            return true;
+        }
+        float k;
+        float b;
+        if (coordinates[1][0] != coordinates[0][0]) {
+            k = (float) (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
+            b = (float) coordinates[0][1] - k * coordinates[0][0];
+            for (int i = 2; i < coordinates.length; i++) {
+                if (!(Math.abs(coordinates[i][1] - (k * coordinates[i][0] + b)) < 10e-6)) {
+                    return false;
+                }
+            }
+        } else {
+            int x = coordinates[0][0];
+            for (int i = 2; i < coordinates.length; i++) {
+                if (coordinates[i][0] != x) return false;
+            }
+        }
+        return true;
+    }
+}
