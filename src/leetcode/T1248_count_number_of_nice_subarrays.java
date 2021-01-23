49c6d98c9ab82b4d8d532f74565d660e11c02100
LAPTOP-HM\holdice <hemu0710@qq.com>
2021-01-23 15:20:18 +0800

    1248. 统计「优美子数组」

---
+++ src/leetcode/T1248_count_number_of_nice_subarrays.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class T1248_count_number_of_nice_subarrays {
+
+    // https://leetcode-cn.com/problems/count-number-of-nice-subarrays/
+
+    public int numberOfSubarrays(int[] nums, int k) {
+        int ans = 0;
+        int odd = 0; //奇数个数
+        int[] mp = new int[nums.length + 1]; //记录有x个奇数的点的位置出现的次数
+        mp[0] = 1;
+        for (int i = 0; i < nums.length; i ++) {
+            if (nums[i] % 2 == 1) odd ++;
+            if (odd >= k)
+                ans += mp[odd - k];
+            mp[odd] += 1;
+        }
+        return ans;
+    }
+}
