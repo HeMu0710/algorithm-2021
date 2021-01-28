2b2ea903c6ac17bfd626f9fec965ba15328cc10c
LAPTOP-HM\holdice <hemu0710@qq.com>
2021-01-28 22:10:34 +0800

    2021/1/28 每日一题 724. 寻找数组的中心索引

---
+++ src/leetcode/T724_find_pivot_index.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class T724_find_pivot_index {
+
+    // https://leetcode-cn.com/problems/find-pivot-index/
+
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return -1;
+        int sum = Arrays.stream(nums).sum();
+        int right = sum - nums[0];
+        int left = 0;
+        for (int i = 0; i < n; i++) {
+            if (right == left) return i;
+            if (i != n - 1) right -= nums[i + 1];
+            left += nums[i];
+        }
+        return -1;
+    }
+}
