01e1bdb4ab3be00eb14eff84783c4c36a9dade1f
LAPTOP-HM\holdice <hemu0710@qq.com>
2021-02-17 14:01:28 +0800

    2021/2/17 每日一题 566. 重塑矩阵

---
+++ src/leetcode/february/T566_reshape_the_matrix.java
@@ -0,0 +1,22 @@
+package leetcode.february;
+
+import java.util.Arrays;
+
+public class T566_reshape_the_matrix {
+
+    // https://leetcode-cn.com/problems/reshape-the-matrix/
+
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int m = nums.length;
+        int n = nums[0].length;
+        if (m * n != r * c) {
+            return nums;
+        }
+
+        int[][] ans = new int[r][c];
+        for (int x = 0; x < m * n; ++x) {
+            ans[x / c][x % c] = nums[x / n][x % n];
+        }
+        return ans;
+    }
+}
