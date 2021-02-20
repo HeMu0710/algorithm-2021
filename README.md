372c84b8c714ba54c1b5420269e8454c459c709a
LAPTOP-HM\holdice <hemu0710@qq.com>
2021-02-20 14:22:27 +0800

    剑指 Offer 39. 数组中出现次数超过一半的数字

--- README.md
+++ README.md
@@ -33,6 +33,7 @@
 | 2021/2/19 | [剑指 Offer 37. 序列化二叉树](https://leetcode-cn.com/problems/xu-lie-hua-er-cha-shu-lcof/) | 层序遍历，包含二叉树完整信息                                 |
 | 2021/2/19 | [剑指 Offer 38. 字符串的排列](https://leetcode-cn.com/problems/zi-fu-chuan-de-pai-lie-lcof/) | DFS+剪枝                                                     |
 | 2021/2/20 | [剑指 Offer 39. 数组中出现次数超过一半的数字](https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof/) | 摩尔投票法                                                   |
+| 2021/2/20 | [697. 数组的度](https://leetcode-cn.com/problems/degree-of-an-array/) | 遍历，保存每个数字出现次数、第一次出现和最后一次出现         |
 
 ## 归档
 
---
+++ src/leetcode/february/T697_degree_of_array.java
@@ -0,0 +1,34 @@
+package leetcode.february;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class T697_degree_of_array {
+
+    // https://leetcode-cn.com/problems/degree-of-an-array/
+
+    public int findShortestSubArray(int[] nums) {
+        int n = nums.length;
+        Map<Integer, int[]> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], new int[]{1, i, i});
+            } else {
+                int[] x = map.get(nums[i]);
+                x[0] ++;
+                x[2] = i;
+            }
+        }
+        int count = 0;
+        int ans = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, int[]> entry : map.entrySet()) {
+            count = Math.max(count, entry.getValue()[0]);
+        }
+        for (Map.Entry<Integer, int[]> entry : map.entrySet()) {
+            if (entry.getValue()[0] == count) {
+                ans = Math.min(ans, entry.getValue()[2] - entry.getValue()[1]);
+            }
+        }
+        return ans + 1;
+    }
+}
