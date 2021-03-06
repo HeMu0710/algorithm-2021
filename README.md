e344a18a628e4c6538d9eec8eeae090284e9731c
LAPTOP-HM\holdice <hemu0710@qq.com>
2021-03-06 15:46:19 +0800

    2021/3/6 503. 下一个更大元素 II 单调栈+循环数组

--- README.md
+++ README.md
@@ -13,6 +13,7 @@
 | 2021/3/4 | [354. 俄罗斯套娃信封问题](https://leetcode-cn.com/problems/russian-doll-envelopes/) | 二维降为一维后，动态规划 |
 | 2021/3/4 | [剑指 Offer 57 - II. 和为s的连续正数序列](https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/) | 滑动窗口 |
 | 2021/3/5 | [232. 用栈实现队列](https://leetcode-cn.com/problems/implement-queue-using-stacks/) | 2个栈解决 |
+| 2021/3/6 | [503. 下一个更大元素 II](https://leetcode-cn.com/problems/next-greater-element-ii/) | 单调栈+循环数组 |
 
 ## 归档
 
--- src/leetcode/march/T503_next_greater_element_Ⅱ.java
+++ src/leetcode/march/T503_next_greater_element_Ⅱ.java
@@ -1,5 +1,10 @@
 package leetcode.march;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class T503_next_greater_element_Ⅱ {
 
     // https://leetcode-cn.com/problems/next-greater-element-ii/
@@ -22,7 +27,16 @@ public class T503_next_greater_element_Ⅱ {
     }
 
     public int[] nextGreaterElements(int[] nums) {
-        //TODO: 单调栈+循环数组
-        return null;
+        int len = nums.length;
+        int[] ans = new int[len];
+        Arrays.fill(ans, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 2 * len; i++) {
+            while (!stack.empty() && nums[stack.peek()] < nums[i % len]) {
+                ans[stack.pop()] = nums[i % len];
+            }
+            stack.push(i % len);
+        }
+        return ans;
     }
 }
